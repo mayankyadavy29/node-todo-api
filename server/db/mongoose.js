ed3802070492d0fac5ded6b38abf56e37084803d
Mayank <mayankyadavy29@gmail.com>
2019-10-15 01:31:25 +0530

    Made more changes..lol

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var addr = "mongodb+srv://mayankyadavy29:<password>@node-todo-api-db-nrdvy.mongodb.net/test?retryWrites=true&w=majority";
+var addr = "mongodb+srv://mayankyadavy29:Allowus123@node-todo-api-db-nrdvy.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(addr || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
