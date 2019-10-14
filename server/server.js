b21d42255b122d6b2739ad802f67adfcc9f16beb
Mayank <mayankyadavy29@gmail.com>
2019-10-14 23:07:02 +0530

    Initial commit of the repository

---
+++ server/server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/Todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+var port = process.env.port || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) =>{
+        res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/todos', (req, res) =>{
+    Todo.find().then((todos) =>{
+        res.send({todos});
+    });
+}, (err) => {
+    res.status(400).send(err);
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectId.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();  
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port 3000 ${port}`);
+});
